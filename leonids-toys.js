6a7b875dd66a3b2684b5802f08d23b4d380bbbc1
kyliepost <kylie.postlethwaite00@gmail.com>
2021-07-15 14:25:32 -0500

    first commit

---
+++ leonids-toys.js
@@ -0,0 +1,20 @@
+const Barbie = {
+    id: 1,
+    name: "Barbie" ,
+    producer: ,
+    quantity: 
+}
+
+const Hot Wheels  = {
+    id: 2,
+    name: ,
+    producer: ,
+    quantity: 
+}
+
+const Frisbee = {
+    id: 3,
+    name: ,
+    producer: ,
+    quantity: 
+}
